7209fc22e03b452d16a3576b4d00fba8c183acee
alfatihtalab <alfatihtalab7@gmail.com>
2022-08-22 08:22:46 +0200

    android kotlin roll dice app

---
+++ app/src/main/java/com/alfatih/mykotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.alfatih.mykotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.digit_textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
